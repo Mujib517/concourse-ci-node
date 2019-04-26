360c9056903009a06929d58461e3233a5f473e48
Mujib <mujib517@gmail.com>
2019-04-26 11:57:01 +0530

    Conditionally failing build

--- ci/scripts/static-analysis.sh
+++ ci/scripts/static-analysis.sh
@@ -6,8 +6,10 @@ ln -s ../node_modules
 
 nodejsscan -d src -o results
 
-cat results.json
+missing_heders = jq -r .total_count.mis results.json
+security_issues = jq -r .total_count.sec results.json
 
-jq -r .total_count.mis results.json
-jq -r .total_count.good results.json
-jq -r .total_count.sec results.json
+if [ "$missing_heders" -ge 0 ]; then exit 1
+if [ "$security_issues" -ge 0 ]; then exit 1
+
+exit 0;
